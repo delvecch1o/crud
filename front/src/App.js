641adfddcb4c249bf50289f716147aedfb4a2389
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-10-14 18:31:18 -0300

    cadastro

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import axios from 'axios';
+
+import Home from './components/layouts/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Redirect } from 'react-router-dom';
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+          
+          <Route path="/login">
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
+          </Route>
+
+          <Route path="/register">
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
+          </Route>
+         
+
+        </Switch>
+      </Router>
+
     </div>
   );
 }
 
 export default App;
+
+/*
+
+ <Route exact path="/login" component={Login} />
+  <Route exact path="/register" component={Register} />
+
+*/
